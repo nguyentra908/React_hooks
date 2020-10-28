44e97f62fd7d69ee50c5000d0819857d9226005f
Tra Thao Nguyen <17520831@gm.uit.edu.vn>
2020-10-28 10:28:49 +0700

    todoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 
+import { useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList]=useState( [
+    { id: 1, title: 'I love Easy Frontend! 😍 ' },
+    { id: 2, title: 'We love Easy Frontend! 🥰 ' },
+    { id: 3, title: 'They love Easy Frontend! 🚀 ' },
+   ]
+   );
+
+   function handleTodoClick(todo) {
+     console.log(todo);
+     const index=todoList.findIndex(x=>x.id==todo.id);
+     if(index<0) return;
+     //khi thay đổi state thì tạo mảng mới
+     //... giữ nguyên mảng cũ
+     const newTodoList=[...todoList];
+     newTodoList.splice(index,1);
+     setTodoList(newTodoList);
+     
+   }
   return (
     <div className="App">
-    <ColorBox/>
+    {/* <ColorBox/> */}
+    <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
     </div>
   );
 }
